cd66bfbbbc81bdd00942d54896ca83acb578c530
Grayson Earle <graysonearle@gmail.com>
2023-09-08 13:30:08 +0200

    deleted some useless files

--- translate-test.py
+++
@@ -1,6 +0,0 @@
-text = "Ein Welpe rennt durch ein Feld"
-
-from googletrans import Translator  
-translator = Translator()
-translation = translator.translate("Der Himmel ist blau und ich mag Bananen", dest='en')
-print(translation.text)
